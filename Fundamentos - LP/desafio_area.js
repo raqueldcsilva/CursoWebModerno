a44b7592ff1554294519966b8c97d6a054259f36
Raqueldcsilva <raquelshdy@gmail.com>
2021-09-11 19:42:15 -0300

    Aula e exercícios da seção 4 do curso

---
+++ Fundamentos - LP/desafio_area.js
@@ -0,0 +1,9 @@
+/*Segundo desafio do curso de Web Moderno:
+Calcular a área usando js*/
+
+let raio = 10;
+let PI = Math.PI;
+let calculoArea = PI * raio * raio;
+
+console.log("O valor da área é: " + calculoArea + " m2")
+
---
+++ Fundamentos - LP/desafio_sentenca_bloco.js
@@ -0,0 +1,27 @@
+/*
+Primeiro desafio, entendendo o funcionamento de sentença e bloco de código em JS:
+ Exibir em um bloco os números 1 3 5 7 9 e em outro os 
+ números 2 4 6 8 10
+*/
+console.log("Início do 1º bloco de código")
+{
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+}
+
+console.log("Fim do 1º bloco de código")
+console.log("==========================================")
+console.log("Início do 2º bloco de código")
+
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
+
+console.log("Fim do 2º bloco de código")
