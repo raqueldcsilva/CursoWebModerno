4648b24991867786ad0fd5956d93ff1ea6b84806
Raqueldcsilva <raquelshdy@gmail.com>
2021-09-30 22:23:07 -0300

    iniciando a aula de OO

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//Código não executável - aula teórica
+
+//Procedural 
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = { // o objeto agrupa comportamentos e atributos
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto 
+
+//Princípios importantes:
+//1. abstração -> simplificar um objeto e trazer para o mundo de software 
+//2. encapsulamento -> ter os detalhes de implementação escondidos, mas mostrar uma interface simples ao usuário para comunicação
+//3. herança (prototype) -> recebemos atributos e comportamentos de um objeto pai
+//4. polimorfismo
